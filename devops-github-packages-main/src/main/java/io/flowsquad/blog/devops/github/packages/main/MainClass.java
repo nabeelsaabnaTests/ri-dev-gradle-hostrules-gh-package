da07081e9feef27a4b72c788961e9bdfc553ff42
DimaMend <145161091+DimaMend@users.noreply.github.com>
2024-02-28 12:28:12 -0600

    Update MainClass.java

--- devops-github-packages-main/src/main/java/io/flowsquad/blog/devops/github/packages/main/MainClass.java
+++ devops-github-packages-main/src/main/java/io/flowsquad/blog/devops/github/packages/main/MainClass.java
@@ -1,6 +1,6 @@
 package io.flowsquad.blog.devops.github.packages.main;
 
-import io.flowsquad.blog.devops.github.packages.library.LibraryClass;
+//import io.flowsquad.blog.devops.github.packages.library.LibraryClass;
 
 public class MainClass {
     public static void main(String[] args) {
